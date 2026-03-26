030f44e5e550f72bb5b84778ce3dfd6d74604129
Morningstar <julianpulidoarias2003@gmail.com>
2026-03-26 08:53:56 -0500

    primer commit ionic

---
+++ PruebaComponentes/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PruebaComponentes',
+  webDir: 'www'
+};
+
+export default config;
